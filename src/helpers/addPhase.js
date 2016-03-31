8c5d3fca12d9b6a3031143139cfe35cecd8cd658
Phillip Gibson <gibson.phillip.j@gmail.com>
2016-03-30 22:27:42 -0400

    Force static analyser to load all phases so package will work in browserify.

--- src/helpers/addPhase.js
+++ src/helpers/addPhase.js
@@ -1,3 +1,7 @@
+require('../phases/play')
+require('../phases/mark')
+require('../phases/end')
+
 export default (state) => {
   const boundPhase = require(`../phases/${state.phase}`)(state)
   Object.keys(boundPhase).forEach((key) => {
--- src/helpers/removePhase.js
+++ src/helpers/removePhase.js
@@ -1,1 +1,5 @@
+require('../phases/play')
+require('../phases/mark')
+require('../phases/end')
+
 export default (state) => Object.keys(require(`../phases/${state.phase}`)(state)).forEach((key) => delete state[key])
